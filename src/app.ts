c731141bc87beb751e1a3e24d8c87d1313517ea4
fambirachimwe <36159141+Fambirachimwe@users.noreply.github.com>
2025-09-24 10:55:21 +0200

    corrected cors error by adding the frontend into the list of origins

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ export function createApp() {
     const app = express();
 
     // app.use(helmet());
-    app.use(cors({ origin: true, credentials: true }));
+    app.use(cors({ origin: ["http://localhost:3000", "http://localhost:4000", "https://uipaerpfinal-frontend-production.up.railway.app/"], credentials: true }));
     app.use(express.json({ limit: "1mb" }));
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
